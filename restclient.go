2d186ad4cf160b942320990a317cd68b76af5f15
Jason McVetta <jason.mcvetta@gmail.com>
2012-12-10 16:44:20 -0800

    Simplification

--- restclient.go
+++ restclient.go
@@ -13,63 +13,96 @@ import (
 	"net/url"
 )
 
-type RestClient struct {
-	client *http.Client
+type Method string
+
+var (
+	GET    = Method("GET")
+	PUT    = Method("PUT")
+	POST   = Method("POST")
+	DELETE = Method("DELETE")
+)
+
+type RestRequest struct {
+	Url     string            // Raw URL string
+	Method  Method            // HTTP method to use 
+	Params  map[string]string // URL parameters for GET requests (ignored otherwise)
+	Headers *http.Header      // HTTP Headers to use (will override defaults)
+	Data    interface{}       // Data to JSON-encode and include with call
+	Result  interface{}       // JSON-encoded data in respose will be unmarshalled into Result
 }
 
-func (r *RestClient) GetUrl(u *url.URL, result interface{}) (status int, err error) {
-	c := call{
-		Url:    u,
-		Method: "GET",
-		Result: result,
+// Client is a REST client.
+type Client struct {
+	HttpClient *http.Client
+}
+
+// New returns a new Client instance.
+func New() Client {
+	return Client{
+		HttpClient: new(http.Client),
 	}
-	return r.rest(&c)
 }
 
-func (r *RestClient) Get(rawurl string, result interface{}) (status int, err error) {
-	u, err := url.Parse(rawurl)
+// Do executes an HTTP REST request
+func (c *Client) Do(r *RestRequest) (status int, err error) {
+	//
+	// Create a URL object from the raw url string.  This will allow us to compose
+	// query parameters programmatically and be guaranteed of a well-formed URL.
+	//
+	u, err := url.Parse(r.Url)
 	if err != nil {
 		return
 	}
-	return r.GetUrl(u, result)
-}
-
-type call struct {
-	Url     *url.URL    // Absolute URL to call
-	Method  string      // HTTP method to use 
-	Content interface{} // Data to JSON-encode and include with call
-	Result  interface{} // JSON-encoded data in respose will be unmarshalled into Result
-}
-
-func (r *RestClient) rest(c *call) (status int, err error) {
-	req, err := http.NewRequest(c.Method, c.Url.String(), nil)
-	if err != nil {
-		return
+	//
+	// If we are making a GET request and the user populated the Params field, then
+	// add the params to the URL's querystring.
+	//
+	if r.Method == GET && r.Params != nil {
+		vals := u.Query()
+		for k, v := range r.Params {
+			vals.Set(k, v)
+		}
+		u.RawQuery = vals.Encode()
 	}
-	if c.Content != nil {
-		// log.Println(pretty.Sprintf("Content: %# v", c.Content))
+	//
+	// Create a Request object; if populated, Data field is JSON encoded as request
+	// body
+	//
+	m := string(r.Method)
+	var req *http.Request
+	if r.Data == nil {
+		req, err = http.NewRequest(m, r.Url, nil)
+	} else {
+		// log.Println(pretty.Sprintf("Content: %# v", r.Content))
 		var b []byte
-		b, err = json.Marshal(c.Content)
-		if err != nil {
+		b, err = json.Marshal(r.Data)
+		if err != nil { // Create a URL object from the raw url string.  This will allow us to compose URL parameters programmatically and be
 			return
 		}
 		buf := bytes.NewBuffer(b)
-		req, err = http.NewRequest(c.Method, c.Url.String(), buf)
-		if err != nil {
-			return
-		}
+		req, err = http.NewRequest(m, r.Url, buf)
 		req.Header.Add("Content-Type", "application/json")
 	}
-	req.Header.Add("Accept", "application/json")
+	if err != nil {
+		return
+	}
+	//
+	// If Accept header is unset, set it for JSON.
+	//
+	if req.Header.Get("Accept") == "" {
+		req.Header.Add("Accept", "application/json")
+	}
 	// log.Println(pretty.Sprintf("Request: %# v", req))
-	resp, err := r.client.Do(req)
+	//
+	// Execute the HTTP request
+	//
+	resp, err := c.HttpClient.Do(req)
 	if err != nil {
 		return
 	}
 	status = resp.StatusCode
 	var data []byte
 	data, err = ioutil.ReadAll(resp.Body)
-	// Ignore unmarshall errors - worst case is, c.Result will be nil
-	err = json.Unmarshal(data, &c.Result)
+	err = json.Unmarshal(data, &r.Result)
 	return
 }
