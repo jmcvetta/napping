ed468b0123a67f805316aeaa2cd2c2cad8ca81a8
Jason McVetta <jason.mcvetta@gmail.com>
2013-03-25 12:13:14 -0700

    Update copyright to include 2013

--- restclient.go
+++ restclient.go
@@ -1,6 +1,7 @@
-// Copyright (c) 2012 Jason McVetta.  This is Free Software, released under the 
-// terms of the GPL v3.  See http://www.gnu.org/copyleft/gpl.html for details.
-// Resist intellectual serfdom - the ownership of ideas is akin to slavery.
+// Copyright (c) 2012-2013 Jason McVetta.  This is Free Software, released
+// under the terms of the GPL v3.  See http://www.gnu.org/copyleft/gpl.html for
+// details.  Resist intellectual serfdom - the ownership of ideas is akin to
+// slavery.
 
 /*
 Package restclient is a client library for interacting with RESTful APIs.
