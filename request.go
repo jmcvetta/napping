127d425b21f2c7a5888cb57c0b1bd19a83b5a5c0
Jason McVetta <jason.mcvetta@gmail.com>
2013-09-18 12:35:22 -0700

    reorganize Request and Opts structs

--- request.go
+++ request.go
@@ -13,21 +13,17 @@ import (
 
 type Opts struct {
 	Userinfo *url.Userinfo // Optional username/password to authenticate this request
-	Params   Params        // URL parameters for GET requests (ignored otherwise)
+	Params   *Params       // URL parameters for GET requests (ignored otherwise)
 	Header   *http.Header  // HTTP Headers to use (will override defaults)
-	//
+
+	// Pointer to data structure.  On error, response from server is
+	// unmarshaled into this structure.
+	Error interface{}
+
 	// OPTIONAL - ExpectedStatus is the HTTP status code we expect the server
 	// to return on a successful request.  If ExpectedStatus is non-zero and
-	// server returns a different code, Client.Do will return a BadStatus error.
-	//
+	// server returns a different code, Send() will return a BadStatus error.
 	ExpectedStatus int
-	//
-	// The following interfaces fields should be populated with *pointers* to
-	// data structures.  Any structure that can be (un)marshalled by the json
-	// package can be used.
-	//
-	Data  interface{} // Data to JSON-encode and POST
-	Error interface{} // Error response is unmarshalled into Error
 }
 
 // A Request describes an HTTP request to be executed, data structures into
@@ -36,17 +32,15 @@ type Opts struct {
 // access to Result and Error objects without needing type assertions.
 type Request struct {
 	Opts
-	Url    string // Raw URL string
-	Method string // HTTP method to use
-	//
-	// The following interfaces fields should be populated with *pointers* to
-	// data structures.  Any structure that can be (un)marshalled by the json
-	// package can be used.
-	//
-	Result interface{} // Successful response is unmarshalled into Result
-	//
+	Url    string      // Raw URL string
+	Method string      // HTTP method to use
+	Data   interface{} // Data to JSON-encode and POST
+
+	// Result is a pointer to a data structure.  On success, response from
+	// server is unmarshalled into Result.
+	Result interface{}
+
 	// The following fields are populated by Send().
-	//
 	Timestamp    time.Time      // Time when HTTP request was sent
 	RawText      string         // Raw text of server response (JSON or otherwise)
 	Status       int            // HTTP status for executed request
