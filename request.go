8e27e7200c97245a62cd456f74e0dbc984fc1bc0
Jason McVetta <jason.mcvetta@gmail.com>
2013-09-25 13:05:43 -0700

    Expose response data as methods on Request object

--- request.go
+++ request.go
@@ -6,6 +6,7 @@
 package napping
 
 import (
+	"encoding/json"
 	"net/http"
 	"net/url"
 	"time"
@@ -16,10 +17,6 @@ type Opts struct {
 	Params   *Params       // URL parameters for GET requests (ignored otherwise)
 	Header   *http.Header  // HTTP Headers to use (will override defaults)
 
-	// Pointer to data structure.  On error, response from server is
-	// unmarshaled into this structure.
-	Error interface{}
-
 	// OPTIONAL - ExpectedStatus is the HTTP status code we expect the server
 	// to return on a successful request.  If ExpectedStatus is non-zero and
 	// server returns a different code, Send() will return a BadStatus error.
@@ -27,9 +24,9 @@ type Opts struct {
 }
 
 // A Request describes an HTTP request to be executed, data structures into
-// which results and errors will be unmarshalled, and the server's response.
-// By using a single object for both the request and the response we allow easy
-// access to Result and Error objects without needing type assertions.
+// which the result will be unmarshalled, and the server's response. By using
+// a  single object for both the request and the response we allow easy access
+// to Result and Error objects without needing type assertions.
 type Request struct {
 	Opts
 	Url    string      // Raw URL string
@@ -41,8 +38,35 @@ type Request struct {
 	Result interface{}
 
 	// The following fields are populated by Send().
-	Timestamp    time.Time      // Time when HTTP request was sent
-	RawText      string         // Raw text of server response (JSON or otherwise)
-	Status       int            // HTTP status for executed request
-	HttpResponse *http.Response // Response object from http package
+	timestamp time.Time      // Time when HTTP request was sent
+	rawText   string         // Body of server's response (JSON or otherwise)
+	status    int            // HTTP status for executed request
+	response  *http.Response // Response object from http package
+}
+
+// Timestamp returns the time when HTTP request was sent.
+func (r *Request) Timestamp() time.Time {
+	return r.timestamp
+}
+
+// RawText returns the body of the server's response as raw text.
+func (r *Request) RawText() string {
+	return r.rawText
+}
+
+// Status returns the HTTP status for the executed request, or 0 if request has
+// not yet been sent.
+func (r *Request) Status() int {
+	return r.status
+}
+
+// HttpResponse returns the underlying Response object from http package.
+func (r *Request) HttpResponse() *http.Response {
+	return r.response
+}
+
+// Unmarshal parses the JSON-encoded data in the server's response, and stores
+// the result in the value pointed to by v.
+func (r *Request) Unmarshall(v interface{}) error {
+	return json.Unmarshal(r.response.Body, v)
 }
