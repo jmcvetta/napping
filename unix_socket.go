ff2447b9db8f5925e4e6a3afc6925a233606cc6d
Anand Patil <anand@senseplatform.com>
2014-04-23 13:08:28 -0400

    Commented code

--- unix_socket.go
+++ unix_socket.go
@@ -11,10 +11,13 @@ import (
 )
 
 type SocketTransport struct {path string}
+
+// The RoundTripper (http://golang.org/pkg/net/http/#RoundTripper) for the socket transport dials the socket
+// each time a request is made.
 func (d SocketTransport) RoundTrip(req *http.Request) (*http.Response, error) {
-  dial, dialErr := net.Dial("unix", d.path)
-  if dialErr != nil {
-    return nil, dialErr
+  dial, err := net.Dial("unix", d.path)
+  if err != nil {
+    return nil, err
   }
   socketClientConn := httputil.NewClientConn(dial, nil)
   defer socketClientConn.Close()
@@ -30,6 +33,12 @@ func isUnixSocket(path string) bool {
   return fi.Mode()&os.ModeType == os.ModeSocket
 }
 
+// Unix urls like unix://var/run/docker.sock/v1.10/images/json contain two parts: the path to the Unix domain
+// socket, /var/run/docker.sock, and the request path: /v1.10/images/json . This function splits out the two
+// parts by walking down the path to / and checking whether each node is a Unix domain socket as opposed to
+// regular file or directory.
+//
+// If no Unix domain sockets are found, an error is returned.
 func LocateSocket(rawPath string) (string, string, error) {
   s := rawPath
   if s[0] != '/' {
@@ -47,5 +56,5 @@ func LocateSocket(rawPath string) (string, string, error) {
     s, p_ = path.Split(s)
     p = path.Join(p_, p)
   }
-  return "", "", errors.New("No Unix socket found in " + rawPath)
+  return "", "", errors.New("No Unix domain socket found in " + rawPath)
 }
