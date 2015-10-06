28d14340c86086b0c2d353b93f6ab7f69e7edef4
Jason McVetta <jason.mcvetta@gmail.com>
2015-10-06 14:26:02 -0700

    simplify pretty()

--- util.go
+++ util.go
@@ -7,21 +7,10 @@ package napping
 
 import (
 	"encoding/json"
-	"fmt"
-	"path/filepath"
-	"runtime"
 )
 
+// pretty pretty-prints an interface using the JSON marshaler
 func pretty(v interface{}) string {
-	// Get source file and line
-	_, file, line, _ := runtime.Caller(1)
-
-	// Get relative filename
-	filename := filepath.Base(file)
-
-	// Convert to JSON
 	b, _ := json.MarshalIndent(v, "", "\t")
-
-	// Make that all pretty together
-	return fmt.Sprintf("%s:%d: \n%s\n", filename, line, string(b))
+	return string(b)
 }
